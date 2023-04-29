3551dd4fbf6e0e70d462c36c2277aa98f89b70ef
Francisco <fsanchezbz@gmail.com>
2023-04-29 22:38:45 +0200

    social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { GrInstagram } from "react-icons/gr";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 export default function Footer() {
   return (
@@ -11,6 +14,14 @@ export default function Footer() {
           marginTop: "4rem",
         }}
       >
+        <div class="d-flex col-12 justify-content-center align-items-center">
+          <GrInstagram style={{ fontSize: "1.5rem", marginRight: "0.5rem" }} />
+          <FaFacebookSquare
+            style={{ fontSize: "1.5rem", marginRight: "0.5rem" }}
+          />
+          <BsLinkedin style={{ fontSize: "1.5rem", marginRight: "0.5rem" }} />
+          <BsGithub style={{ fontSize: "1.5rem", marginRight: "0.5rem" }} />
+        </div>
         <div className="d-flex col-12 justify-content-center align-items-center">
           <p className="text-black">FJB - Rocket Team Work GmbH © Copyright</p>
         </div>
